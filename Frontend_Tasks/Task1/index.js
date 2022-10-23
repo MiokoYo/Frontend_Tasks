13faab50de4703f2be79f4b0c269ebbb71e4d744
MiokoYo <78951297+MiokoYo@users.noreply.github.com>
2022-10-23 18:27:10 +1000

    Add files via upload

---
+++ Frontend_Tasks/Task1/index.js
@@ -0,0 +1,10 @@
+let block1 = document.getElementsByClassName('block1');
+
+const block2 = document.getElementsByClassName('block2');
+const block3 = document.getElementsByClassName('block3');
+const block4 = document.getElementsByClassName('block4');
+const block5 = document.getElementsByClassName('block5');
+
+const colors = ['#0149cd', '#088ffd', '#2e0617', '#a80d14', '#3f1b98'];
+
+block1.style.background = 'red';
